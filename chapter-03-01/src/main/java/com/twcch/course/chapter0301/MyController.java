5abfce79f4ebc3be58ce54bd2f668b3728e71557
謝志謙 <twcch1218@gmail.com>
2023-12-09 10:46:22 +0800

    更新 Chapter-03-01

--- chapter-03-01/src/main/java/com/twcch/course/chapter0301/MyController.java
+++ chapter-03-01/src/main/java/com/twcch/course/chapter0301/MyController.java
@@ -6,7 +6,7 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class MyController {
 
-    @GetMapping("/hello")
+    @GetMapping("/")
     public String hello() {
         return "Hello Spring Boot";
     }
